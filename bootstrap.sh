95d021c5aed11abf03bbadf8e58e795431ad06f5
elongeau <emmanuel.longeau@gmail.com>
2015-10-06 11:08:36 +0200

    Add command to make it work under Unix
    * use fromdos to convert character from dos to unix
    * git clone Vundle (plugin manager) and gruvbox (theme)
    * add an helper message to install the vim's plugins

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,11 +6,19 @@ for file in $DOTFILES/*.symlink
 do
 	filename=$(basename $file)
 	filename=".${filename%.*}"
-  echo "Création du lien symbolique $filename pour : $file"
-  if [ -e $filename  ] 
-  then
-    rm -rf $HOME/$filename
-  fi
+	echo "Création du lien symbolique $filename pour : $file"
+	if [ -e $filename ];then
+		rm -rf $HOME/$filename
+	fi
 
+	fromdos $file
 	ln -s $file $HOME/$filename
 done
+
+# install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# install gruvbox theme
+git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
+
+echo "You need to run : \nsudo fromdos ~/.vim/bundle/**/*; vim +PluginInstall +qall; sudo fromdos ~/.vim/bundle/**/*"

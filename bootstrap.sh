ea8db747317df7dab1e6727bd5825a9902d92e48
elongeau <emmanuel.longeau@gmail.com>
2015-10-14 09:51:40 +0200

    Close #17 : correct error on bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,31 @@
 DOTFILES=$(pwd)
-cd ~
-HOME=$(pwd)
+VUNDLE_DIR="~/.vim/bundle/Vundle.vim"
+THEME_DIR="~/.vim/bundle/gruvbox"
 
 for file in $DOTFILES/*.symlink
 do
 	filename=$(basename $file)
 	filename=".${filename%.*}"
 	echo "Création du lien symbolique $filename pour : $file"
-	if [ -e $filename ];then
-		rm -rf $HOME/$filename
+	if [ -e ~/$filename ];then
+		rm -rf ~/$filename
 	fi
 
-	fromdos $file
-	ln -s $file $HOME/$filename
+  if type fromdos &> /dev/null; then
+    fromdos $file
+  fi
+
+	ln -s $file ~/$filename
 done
 
-# install Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [[ -z $VUNDLE_DIR ]]; then
+  # install Vundle
+  git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_DIR
+fi
 
-# install gruvbox theme
-git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
+if [[ -z $VUNDLE_DIR ]]; then
+  # install gruvbox theme
+  git clone https://github.com/morhetz/gruvbox.git $THEME_DIR
+fi
 
 echo "You need to run : \nsudo fromdos ~/.vim/bundle/**/*; vim +PluginInstall +qall; sudo fromdos ~/.vim/bundle/**/*"
